9942aeec3fd837993b7963034170a188cc3b4fe3
hmw <hwm@163.com>
2022-06-09 17:26:13 +0100

    added png to skeleton

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
       <h1 data-testid="header">IS THIS DEPLOYED?!</h1>
+      <script>
+        
+      var img = document.createElement("img");
+      img.src = "drpLandingPage.png";
+
+      var div = document.getElementById("x");
+      div.appendChild(img);
+      div.setAttribute("style", "text-align:center");
+
+      </script>
     </div>
   );
 }
