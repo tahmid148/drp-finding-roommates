1b5184776f44dc0970b4f0662e9ea86bfe555462
tahmid148 <tahmidrahman2812@outlook.com>
2022-06-10 12:04:53 +0100

    clean up

--- src/src/components/App/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test("Header renders with correct text", () => {
-  const component = render(<App />);
-  const headerElem = component.getByTestId("header")
-
-  // expect(headerElem.textContent).toBe("IS THIS DEPLOYED?!")
-})
