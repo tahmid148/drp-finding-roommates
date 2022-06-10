ff171e62960a63e0cd667626d5b19e5bdbdc1e46
ameen <ameen.izhac@gmail.com>
2022-06-10 05:10:52 +0100

    connected front and back end

---
+++ client/src/components/App/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test("Header renders with correct text", () => {
+  const component = render(<App />);
+  const headerElem = component.getByTestId("header")
+
+  // expect(headerElem.textContent).toBe("IS THIS DEPLOYED?!")
+})
