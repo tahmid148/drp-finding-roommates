73dfba0e7bb622da83d33cc4b245682d35879ecd
Ibrahim Ibrahim <ii83326@gmail.com>
2022-06-12 23:58:47 +0100

    Updated frontend

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import InputText from '../InputText/InputText';
 import LandingPage from '../LandingPage/LandingPage';
+import profiles from './example-profiles.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 data-testid="header">ROOMB</h1>
+      <nav className='navbar'>
+        <p data-testid="header" className='nav-header'>Roomb</p>
+      </nav>
+      <div className='tagline-container'>
+        <h3 className='tagline'>Finding the best roommate for <span>you</span>.</h3>
+        <img src={profiles} alt='Example Profiles' className='profiles_img'/>
+      </div>
+      <div className='btn-container'>
+        <button className='get-started-btn'>Begin Your Journey</button>
+        <button className='find-out-more-btn'>Find Out More</button>
+      </div>
       <InputText/>
-      <LandingPage/>
     </div>
   );
 }
