5cbac56bce8554690ab4464f61c7c5cc929ffd12
tahmid148 <tahmidrahman2812@outlook.com>
2022-06-15 00:11:09 +0100

    feat: added background colour to webpage

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import { NavigationBar } from '../NavigationBar/NavigationBar';
 
-function App() {
-  return (
-    <NavigationBar />
-  );
+export class App extends React.Component {
+  render() {
+    return (
+      <div className='page'>
+        <NavigationBar />
+      </div>
+    );
+  }
 }
-
-export default App;
