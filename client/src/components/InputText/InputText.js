ad2154179bf9904328df4963ee1110afcbccaa72
tahmid148 <tahmidrahman2812@outlook.com>
2022-06-14 22:14:41 +0100

    refactor: brought back old src into client side

---
+++ client/src/components/InputText/InputText.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const InputText = () => {
+  return (
+      <div className="form">
+      <form action="/" method="POST">
+        <div className="input-container">
+          <label>Area </label>
+          <input type="text" name="area" required />
+        </div>
+        <div className="input-container">
+          <label>Occupation </label>
+          <input type="text" name="occupation" required />
+        </div>
+        <div className="input-container">
+          <label>Minimum Rent Price </label>
+          <input type="text" name="minRentPrice" required />
+        </div>
+        <div className="input-container">
+          <label>Maximum Rent Price </label>
+          <input type="text" name="maxRentPrice" required />
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default InputText
