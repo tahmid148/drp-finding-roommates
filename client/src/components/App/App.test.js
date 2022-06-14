04f9a709050151096a09515aa19d35d13f17e840
tahmid148 <tahmidrahman2812@outlook.com>
2022-06-15 00:15:34 +0100

    fix: fixed import in App.test

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { App } from './App';
 
 test("Header renders with correct text", () => {
   const component = render(<App />);
