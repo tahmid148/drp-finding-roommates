19734183423fcd9237f7617f35b0b248d6244b4e
tahmid148 <tahmidrahman2812@outlook.com>
2022-06-15 18:17:52 +0100

    feat: implementation of your profile fill out form begun

--- client/src/components/YourProfile/YourProfile.js
+++ client/src/components/YourProfile/YourProfile.js
@@ -5,28 +5,28 @@ import './YourProfile.css';
 export class YourProfile extends React.Component {
     render() {
         return (
-            <div>
+            <div className='page'>
                 <nav className='navbar'>
                     <p className='create-profile'>
                         <span className='to-orange'>Create</span> <span className='you'>your</span> <span className='to-orange'>profile</span>
                     </p>
-
                 </nav>
-                <section>
+
+                <section className='fill-out-form'>
                     <div>
-                        <span className='question-text'>Full Name</span>
+                        <span className='question-text name'>Full Name</span>
                         <input className='answer-text' type='text' value='Name'></input>
-                        <span className='question-text'>Age</span>
-                        <input type='number' value=''></input>
-                        <span className='question-text'>Gender</span>
-                        <input type='text' value=''></input>
+                        <span className='question-text age'>Age</span>
+                        <input className='answer-text' type='number' value=''></input>
+                        <span className='question-text gender'>Gender</span>
+                        <input className='answer-text' type='text' value=''></input>
                     </div>
                     <div>
-                        <span className='question-text'>About You</span>
-                        <span className='question-text'>Weekly Budget Range £</span>
-                        <input type='number' value='20'></input>
+                        <span className='question-text about'>About You</span>
+                        <span className='question-text budget'>Weekly Budget Range £</span>
+                        <input className='answer-text budget1' type='number' value='20'></input>
                         <span className='question-text'>to £</span>
-                        <input type='number' value='100'></input>
+                        <input className='answer-text budget2' type='number' value='100'></input>
                     </div>
                     <input className='answer-text' type='text' value='I am an alcoholic...'></input>
                 </section>
