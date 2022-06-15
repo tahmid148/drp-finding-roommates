96a575452b9f873270d8d63cd69f15903b8553e0
hmw <hwm@163.com>
2022-06-15 14:13:39 +0100

    feat: added html code for profile page

--- client/src/components/YourProfile/YourProfile.js
+++ client/src/components/YourProfile/YourProfile.js
@@ -6,7 +6,29 @@ export class YourProfile extends React.Component {
     render() {
         return (
             <div>
-                Your Profile
+                <header>
+                    <div>
+                        <p>Create Your Profile</p>
+                    </div>
+                </header>
+                <section>
+                    <div>
+                        <span>Full Name</span>
+                        <input type='text' value='Name'></input>
+                        <span>Age</span>
+                        <input type='number' value=''></input>
+                        <span>Gender</span>
+                        <input type='text' value=''></input>
+                    </div>
+                    <div>
+                        <span>About You</span>
+                        <span>Weekly Budget Range £</span>
+                        <input type='number' value='20'></input>
+                        <span>to £</span>
+                        <input type='number' value='100'></input>
+                    </div>
+                    <input type='text' value='I am an alcoholic...'></input>
+                </section>
                 <Link to='/roommate-search'>
                     <button>
                         Next 
