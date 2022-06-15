638f0f09423a5ef2f0208036f01969323b0f9ac0
Ibrahim Ibrahim <ii83326@gmail.com>
2022-06-15 16:11:13 +0100

    feat: Styling to tagline component + locally storing images

--- client/src/components/Tagline/Tagline.js
+++ client/src/components/Tagline/Tagline.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Tagline.css';
+import profile1 from './eg-profile-1.svg'
+import profile2 from './eg-profile-2.svg'
+import profile3 from './eg-profile-3.svg'
 
 export class Tagline extends React.Component {
     render() {
         return (
             <div className='tagline-container'>
                 <h3 className='tagline'>Finding the best <br></br> roommate for <br></br><span className='you'>you</span>.</h3>
-                <img className='profiles' src='https://uc6351d2f3774a3e1a749e3a1277.previews.dropboxusercontent.com/p/thumb/ABkdD1L-rj5SA71skbyg-7meUtv_UfL1-bXH2ACoaiEoarLD03VTpB8TfFc3AEdm0BufmgCJePlPfg0bx9muS5RULRizSX-mc0PRDGsKCQ8wKijfJCP7VNp72pB34Oz5UYVxfNXgsiFobvGyQL5xUWCo46yrNgAjRWaGQvIzAEiJyta0WhIBYX0SKoTHOBLU2x5YceK0PEP2ktloIsj1P08WMJFPvY2x-50DjcmvewXcx2l4-Z7ie08FAl99GrwZM553rrz5PqAnNDmA5BTJN7usyOzKZolLEz-gHXSKQ6grEtTfEEWlWLd12b8k-rLm0KpNQ7kty8etztbooBfy5jtmWdCbYdHreaM15Ka4KpVhh-IJMaPzZJEfY3ceTOVdB4C-wY_gJEUyIFlCa5lUSGegKDlocO0_iEb2ei7KoGWeCg/p.png' alt='peoples profiles'></img>
+                <div className='profile-container'>
+                    <img className='profiles' src={profile1} alt='peoples profiles'></img>
+                    <img className='profiles' src={profile2} alt='peoples profiles'></img>
+                    <img className='profiles' src={profile3} alt='peoples profiles'></img>
+                </div>
             </div>
         );
     }
