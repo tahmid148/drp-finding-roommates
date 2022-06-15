fdee637333ca411beeaab294cdf916c0f2198839
tahmid148 <tahmidrahman2812@outlook.com>
2022-06-15 02:35:10 +0100

    feat: information boxes filled

--- client/src/components/InformationBoxes/InformationBoxes.js
+++ client/src/components/InformationBoxes/InformationBoxes.js
@@ -11,14 +11,12 @@ export class InformationBoxes extends React.Component {
                      <p className='helpingText'>Students often find themselves in a situation where they are inconvenienced by those they are living with. We thought making this app would help find the right roommates to live with, to make student life easier.</p>
                 </div>
                 <div className='box2'>
-                <img className='image1' src='https://uce122b29fb4a65f582e31b28ea3.previews.dropboxusercontent.com/p/thumb/ABmiEXSAd_5lvz7RYdFiW34cOqxJ133qXz2uaKlBA9WE6-xif-QovmtC0jnTUevnCx1z97l5go5kCfInHYZLK05jsdHgMsKQXutDvITJvLhQyx01h3UDDVXWHJK7QdpukSHZzDtaBhezeqP7yxzvA23HMTpxKtJaHK6FZbyJfauNgHxtN9gI7egrK3TQ-oS4fCLOUyY6KPrAIMDbHvCDIEDhuddp7vJTrqRZ7lD-Gsb1krrcn5COY9-_gE_8ETuLdhqBwVxogGUYu7N_0MosRJUGdt_9v9LQYlJTKkMOGarJXZ9GA6aakZ29N3GOAOmmxaKhPJaZUV4g-yeoeo-jSizVki58URQq4AQQ0i1XnniQAqpitHBXSM3EThZt2IxSHPSeoQ4_0oApcXykx2ihIIvaH1-XaYrh6QQVnIsAx05qyQ/p.png' alt='People'></img>
-                     <p className='helpingHeader'>Helping you find the best for you.</p>
-                     <p className='helpingText'>Students often find themselves in a situation where they are inconvenienced by those they are living with. We thought making this app would help find the right roommates to live with, to make student life easier.</p>
+                    <p className='helpingHeader'>Easy as 1, 2, 3</p>
+                    <img className='image-123' src='https://uc79aa42c57ecc766147690347ef.previews.dropboxusercontent.com/p/thumb/ABlHLzpUWWtUlMB3XikvFGbCyxXr9FOJqPfRIgDFXP3GZyfSLP8d5LkFVYbcD3B8ahqTyr2YssCusWpJL7YTVsA1Ni7SYTiyXTUC4nYVXo1s7J2KIJ4G2ohCm7MhNSI9iPpSR4a0qguL5QC4IJMeuLGPaXV4Ncj--vGmjbwjK_ndbDkWKz_MQCKEX-vXqezTzCz7OruzTKSBT_eEnbb2x36Gig96ru5mFFwFUewsgw03FtX-JG_Pc4j70UmIf4Yc5chwvRwxjMDwENOYaDnjhLK51-IBzTDeE8XeWOb-lwcB0FODEVPYtiMQ8pV6oSDUOLl432oN3TRBZ8cm6tCYAzLhZzr7I7vh1bu3S8sqsG-1okzr926gN9O-1Vfe3oVAUFGs6JDpLqA5EOfssfvc5Xvzvjyi0ONDXQpEqr2RejU_NA/p.png' alt='People'></img>
+
                 </div>
                 <div className='box3'>
-                <img className='image1' src='https://uce122b29fb4a65f582e31b28ea3.previews.dropboxusercontent.com/p/thumb/ABmiEXSAd_5lvz7RYdFiW34cOqxJ133qXz2uaKlBA9WE6-xif-QovmtC0jnTUevnCx1z97l5go5kCfInHYZLK05jsdHgMsKQXutDvITJvLhQyx01h3UDDVXWHJK7QdpukSHZzDtaBhezeqP7yxzvA23HMTpxKtJaHK6FZbyJfauNgHxtN9gI7egrK3TQ-oS4fCLOUyY6KPrAIMDbHvCDIEDhuddp7vJTrqRZ7lD-Gsb1krrcn5COY9-_gE_8ETuLdhqBwVxogGUYu7N_0MosRJUGdt_9v9LQYlJTKkMOGarJXZ9GA6aakZ29N3GOAOmmxaKhPJaZUV4g-yeoeo-jSizVki58URQq4AQQ0i1XnniQAqpitHBXSM3EThZt2IxSHPSeoQ4_0oApcXykx2ihIIvaH1-XaYrh6QQVnIsAx05qyQ/p.png' alt='People'></img>
-                     <p className='helpingHeader'>Helping you find the best for you.</p>
-                     <p className='helpingText'>Students often find themselves in a situation where they are inconvenienced by those they are living with. We thought making this app would help find the right roommates to live with, to make student life easier.</p>
+                    <img className='image-3' src='https://uc30bd0e9fa354e12af486e7fc09.previews.dropboxusercontent.com/p/thumb/ABlGc427RPqv83nvlcYMnekLzxhHE6FmOZiDg8a8Pz11A_pOGBcoEmsscCKOI807hvDYhjjegardblPe2aiZNQdEn-c8xgD8CjY8Zx4QMgwY-9fyxh5NNm5ZNBQgNWqolnQt_R7iuKwg-uqizMVRSJo2VlspvspG5lrSvIl4BmahJGUvx1tEhMMVoet3url351PND6DjnOyBgAdZmEvD66L1TVHQ6CDpuDbRY_BHuCK7U4vWbR2CRhp0U2Aj0soao0_XOdH5durTFZEWLyVHDMqUsAKQHnFiJWm2LVG8Roea1iaeUC9fC80wvJO8T0pEdm9VUpRoTrHXK7hJ-PARhmePL7tqn3AE4yncF8UqpRmDqi-LGP22-HNU4MbhhM76cYHMNjj-jFPuciEwl6ZO72yR181z3WCcoACus9APQpFkIQ/p.png'></img>
                 </div>  
             </div>
           
