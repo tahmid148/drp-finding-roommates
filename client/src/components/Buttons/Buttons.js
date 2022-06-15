d00cb2c2a473c38708c4113fda9fbeb39390b263
tahmid148 <tahmidrahman2812@outlook.com>
2022-06-15 12:03:17 +0100

    feat: find out more button scrolls down to information boxes on click

--- client/src/components/Buttons/Buttons.js
+++ client/src/components/Buttons/Buttons.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { forwardRef, useRef } from 'react';
 import './Buttons.css'
 
 export class Buttons extends React.Component {
+    scrollToInfo() {
+        const infoElement = document.getElementById('info');
+        infoElement.scrollIntoView({ behavior: 'smooth' });
+    }
+
     render() {
         return (
             <div className='btn-container'>
                 <button className='begin-btn'>
                     Begin Your Journey
                 </button>
-                <button className='more-btn'>
+                <button className='more-btn' onClick={this.scrollToInfo}>
                     Find Out More
                 </button>
             </div>
--- client/src/components/InformationBoxes/InformationBoxes.js
+++ client/src/components/InformationBoxes/InformationBoxes.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { forwardRef, useRef } from 'react';
 import './InformationBoxes.css';
 
 export class InformationBoxes extends React.Component {
     render() {
         return (
-            <div className='box-container'>
+            <div id='info' className='box-container'>
                 <div className='box1'>
                      <img className='image1' src='https://uce122b29fb4a65f582e31b28ea3.previews.dropboxusercontent.com/p/thumb/ABmiEXSAd_5lvz7RYdFiW34cOqxJ133qXz2uaKlBA9WE6-xif-QovmtC0jnTUevnCx1z97l5go5kCfInHYZLK05jsdHgMsKQXutDvITJvLhQyx01h3UDDVXWHJK7QdpukSHZzDtaBhezeqP7yxzvA23HMTpxKtJaHK6FZbyJfauNgHxtN9gI7egrK3TQ-oS4fCLOUyY6KPrAIMDbHvCDIEDhuddp7vJTrqRZ7lD-Gsb1krrcn5COY9-_gE_8ETuLdhqBwVxogGUYu7N_0MosRJUGdt_9v9LQYlJTKkMOGarJXZ9GA6aakZ29N3GOAOmmxaKhPJaZUV4g-yeoeo-jSizVki58URQq4AQQ0i1XnniQAqpitHBXSM3EThZt2IxSHPSeoQ4_0oApcXykx2ihIIvaH1-XaYrh6QQVnIsAx05qyQ/p.png' alt='People'></img>
                      <p className='helpingHeader'>Helping you find the best for you.</p>
