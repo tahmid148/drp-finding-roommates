15fd5735f263fd1ec6bba0b0b2e319e8c51ac352
tahmid148 <tahmidrahman2812@outlook.com>
2022-06-15 02:25:48 +0100

    feat: added photo cards

--- client/src/components/Tagline/Tagline.js
+++ client/src/components/Tagline/Tagline.js
@@ -6,6 +6,7 @@ export class Tagline extends React.Component {
         return (
             <div className='tagline-container'>
                 <h3 className='tagline'>Finding the best <br></br> roommate for <br></br><span className='you'>you</span>.</h3>
+                <img className='profiles' src='https://uc6351d2f3774a3e1a749e3a1277.previews.dropboxusercontent.com/p/thumb/ABkdD1L-rj5SA71skbyg-7meUtv_UfL1-bXH2ACoaiEoarLD03VTpB8TfFc3AEdm0BufmgCJePlPfg0bx9muS5RULRizSX-mc0PRDGsKCQ8wKijfJCP7VNp72pB34Oz5UYVxfNXgsiFobvGyQL5xUWCo46yrNgAjRWaGQvIzAEiJyta0WhIBYX0SKoTHOBLU2x5YceK0PEP2ktloIsj1P08WMJFPvY2x-50DjcmvewXcx2l4-Z7ie08FAl99GrwZM553rrz5PqAnNDmA5BTJN7usyOzKZolLEz-gHXSKQ6grEtTfEEWlWLd12b8k-rLm0KpNQ7kty8etztbooBfy5jtmWdCbYdHreaM15Ka4KpVhh-IJMaPzZJEfY3ceTOVdB4C-wY_gJEUyIFlCa5lUSGegKDlocO0_iEb2ei7KoGWeCg/p.png' alt='peoples profiles'></img>
             </div>
         );
     }
